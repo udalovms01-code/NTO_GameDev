48760f8c508f56a1a789c84f1470fa9b6dd71317
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2025-11-26 18:26:57 +0300

    Add GlobalInstaller script and update .gitignore
    Added GlobalInstaller.cs and its meta files for Zenject dependency injection setup in Unity. Also updated .gitignore to exclude .idea/ directory.

---
+++ Assets/Scripts/GlobalInstaller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace BalatroFeel.Scripts
+{
+    public class GlobalInstaller : MonoInstaller
+    {
+
+        //[SerializeField] private GameContext _gameContext;
+        
+        public override void InstallBindings()
+        {
+            //Container.BindInterfacesAndSelfTo<>().AsSingle(); 
+            //Container.Bind<GameContext>().FromInstance(_gameContext);
+            
+            
+        }
+    }
+}
