f3a0cad477d3cbe91275ac7d9e510f6ef29de946
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2025-12-05 18:57:16 +0300

    commit

---
+++ Assets/Scripts/UI/Visuals/UnderlineHover.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI.Visuals
+{
+    public class UnderlineHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public RectTransform underline;
+        public float animSpeed = 8f;
+
+        private float targetWidth;
+        private float fullWidth;
+
+        private void Start()
+        {
+            if (underline == null)
+                Debug.LogError("Assign underline RectTransform!");
+
+            fullWidth = transform.GetComponent<RectTransform>().rect.width;
+            underline.sizeDelta = new Vector2(0, underline.sizeDelta.y);
+        }
+
+        private void Update()
+        {
+            float newWidth = Mathf.Lerp(underline.sizeDelta.x, targetWidth, Time.deltaTime * animSpeed);
+            underline.sizeDelta = new Vector2(newWidth, underline.sizeDelta.y);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            targetWidth = fullWidth;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            targetWidth = 0f;
+        }
+    }
+}
