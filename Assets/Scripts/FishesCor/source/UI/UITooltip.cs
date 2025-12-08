fed6474dce1708701ba0540167a7776aaeb9d375
DukeOfTimes <udalovms01@gmail.com>
2025-12-08 06:09:21 +0300

    tooltip debug

--- Assets/Scripts/FishesCor/source/UI/UITooltip.cs
+++ Assets/Scripts/FishesCor/source/UI/UITooltip.cs
@@ -31,6 +31,7 @@ public class UITooltip : MonoBehaviour
         _rectTransform.anchoredPosition = G.hud.MousePos();
         if (state.model.Is<TagFishView>(out var fv))
         {
+            Debug.Log((fv.name, LocalizationManager.Get(fv.name)));
             name.text = LocalizationManager.Get(fv.name);
             description.text = fv.description;
         }    
