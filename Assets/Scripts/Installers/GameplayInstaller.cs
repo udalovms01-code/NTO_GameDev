1fd8ff73d4807655d6a14be2f52fd1b841ba619b
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2025-12-08 09:35:29 +0300

    Add music controller for game states

--- Assets/Scripts/Installers/GameplayInstaller.cs
+++ Assets/Scripts/Installers/GameplayInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Gameplay;
+using Audio;
+using Gameplay;
 using UnityEngine;
 using Zenject;
 
@@ -7,13 +8,18 @@ namespace Installers
     public class GameplayInstaller : MonoInstaller
     {
         [SerializeField] private GameplaySettings _gameplaySettings;
-        
+
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<GameStateService>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<CameraSwitch>().AsSingle();
             Container.Bind<GameplaySettings>().FromInstance(_gameplaySettings).AsSingle();
             Container.BindInterfacesAndSelfTo<PlayerInteraction>().AsSingle().NonLazy();
+            Container.BindInterfacesAndSelfTo<GameStateMusicController>()
+                .FromNewComponentOnNewGameObject()
+                .WithGameObjectName("GameStateMusic")
+                .AsSingle()
+                .NonLazy();
         }
     }
 }
